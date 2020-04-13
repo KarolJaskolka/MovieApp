80fbdf6127898129ee019357985f9b037798281c
KarolJaskolka <jaskolka.karol@gmail.com>
2020-04-14 00:01:49 +0200

    update app components scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './modules/layout/home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
+  {
+    path: '', 
+    redirectTo: 'home', 
+    pathMatch: 'full'
+  },
+  {
+    path: 'home', 
+    component: HomeComponent
+  },
   {
     path: 'user',
     loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)
