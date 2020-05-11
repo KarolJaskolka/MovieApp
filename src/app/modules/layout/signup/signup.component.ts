a65f7ee944ba47a23b7bb0ee7e23b94d5b997f30
KarolJaskolka <jaskolka.karol@gmail.com>
2020-05-11 16:20:03 +0200

    update signup

--- src/app/modules/layout/signup/signup.component.ts
+++ src/app/modules/layout/signup/signup.component.ts
@@ -25,38 +25,27 @@ export class SignUpComponent implements OnInit, OnDestroy {
     ngOnInit() {
         const emailPattern = '^[a-zA-Z0-9_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$';
         const passwordPattern = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,40}$';
-        const datePattern = '^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$';
+        //const datePattern = '^[1-2][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$';
 
         this.signUpForm = this.formBuilder.group({
             login: [null, Validators.required],
             password: [null, [Validators.required, Validators.pattern(passwordPattern)]],
-            firstName: [null, Validators.required],
-            lastName: [null, Validators.required],
-            email: [null, [Validators.required, Validators.pattern(emailPattern)]],
-            phone: [null],
-            birth: [null, [Validators.required, Validators.pattern(datePattern)]]
+            firstname: [null, Validators.required],
+            lastname: [null, Validators.required],
+            email: [null, [Validators.required, Validators.pattern(emailPattern)]]
+            //phone: [null],
+            //birth: [null, [Validators.required, Validators.pattern(datePattern)]]
         });
     }
 
     signUp(event){
         event.preventDefault();
-        this.getData();
-        this.userSubscription = this.authService.register(this.newUser).subscribe(data => {
+        this.userSubscription = this.authService.register(this.signUpForm.value).subscribe(data => {
             this.toastrService.success('Done!');
             this.router.navigate(['/form/success']);
         }, error => {
             this.registerError = true;
         });
-    }
-
-    getData() {
-        this.newUser.login = this.signUpForm.get('login').value;
-        this.newUser.password = this.signUpForm.get('password').value;
-        this.newUser.firstname = this.signUpForm.get('firstName').value;
-        this.newUser.lastname = this.signUpForm.get('lastName').value;
-        this.newUser.email = this.signUpForm.get('email').value;
-        this.newUser.phone = this.signUpForm.get('phone').value;
-        this.newUser.birth = this.signUpForm.get('birth').value;
     }
 
     ngOnDestroy(){
